6e4be05840e668c66045a63455df9651f15f9827
Dmytro Roienko <droie@softserveinc.com>
2018-08-16 13:31:45 +0300

    annotation-config added

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -19,6 +19,9 @@ public class App {
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App) ctx.getBean("app");
 
+        Client client = ctx.getBean(Client.class);
+        System.out.println("Client says: " + client.getGreeting());
+
         Event event = ctx.getBean(Event.class);
         app.logEvent(EventType.INFO, event,"Some event for user 1");
 
